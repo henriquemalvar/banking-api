82f71a9ce8444573e2adf83bc69ffff8025407dd
Henrique Malvar <henriquemalvar@gmail.com>
2024-06-25 12:46:24 -0300

    refactor: Update account routes to use prefix for '/accounts' and add balance endpoint

--- routes/api.php
+++ routes/api.php
@@ -2,17 +2,24 @@
 
 use App\Http\Controllers\AccountController;
 use App\Http\Controllers\TransactionController;
+use App\Services\ExchangeRateService;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/hello', function () {
     return response()->json(['message' => 'Hello World!']);
 });
 
-Route::post('/accounts', [AccountController::class, 'create']);
+Route::prefix('/accounts')->group(function () {
+    Route::post('/', [AccountController::class, 'create']);
 
-Route::post('/accounts/{accountNumber}/deposit', [TransactionController::class, 'deposit']);
+    Route::post('/{accountNumber}/deposit', [TransactionController::class, 'deposit']);
 
-Route::post('/accounts/{accountNumber}/withdraw', [TransactionController::class, 'withdraw']);
+    Route::post('/{accountNumber}/withdraw', [TransactionController::class, 'withdraw']);
 
-Route::get('/accounts/{accountNumber}/balance', [TransactionController::class, 'balance']);
+    Route::get('/{accountNumber}/balance', [TransactionController::class, 'balance']);
+});
 
+
+Route::fallback(function () {
+    return response()->json(['message' => 'Not Found'], 404);
+});
